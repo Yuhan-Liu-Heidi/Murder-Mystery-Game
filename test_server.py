a9d525797436cf0acef8429dfa597c47529150e9
Yuhan Liu <liuyuhanheidi@163.com>
2019-12-21 16:41:46 -0500

    yuhan - adjusted test: verify user

--- test_server.py
+++ test_server.py
@@ -2,8 +2,8 @@ import pytest
 
 
 @pytest.mark.parametrize("ID, p, expected", [("Heidi", "0326", None),
-                                    ("Heidi", "326", "密码不正确"),
-                                    ("Heid", "0326", "用户名不正确")])
+                                             ("Heidi", "326", "密码不正确"),
+                                             ("Heid", "0326", "用户名不正确")])
 def test_verify_user(ID, p, expected):
     from server import verify_user
     result = verify_user(ID, p)
