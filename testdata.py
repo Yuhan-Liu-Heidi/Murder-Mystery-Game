595e86d605fa723b6476dec02e7a0e0c6ac6b351
Lucy Liang <lucy.liang@duke.edu>
2019-12-25 17:58:12 -0500

    separated game dictionary and track dictionary. export game dictionary into sample_game.json

--- testdata.py
+++ testdata.py
@@ -1,3 +1,5 @@
+import json
+
 user = {'01': {'pw': '111',
                'char': {'name': None, 'story': None},
                'ap': 4,
@@ -30,10 +32,18 @@ user = {'01': {'pw': '111',
                }
         }
 
-game = {'chars': {'A': False, 'B': False, 'C': False},
+track = {'chars': {'A': False, 'B': False, 'C': False},
+         'publicized_clue': {'p1': [['this is 1/2 clue at p1 ele1'],
+                                    ['this is 1/1 clue at p1 ele2']],
+                             'p2': [['this is 1/1 clue at p2 ele1']]},
+         }
+
+game = {'chars': ['A', 'B', 'C'],
         'clues': {'round1': {'p1': [['clue1 at p1 w/ hidden',
                                      'hidden of clue1 at p1'],
+                             # w/ hidden, list with 2 elements
                                     ['clue2 at p1 w/o hidden']],
+                             # w/o hidden, list with 1 element
                              'p2': [['clue1 at p2 w/o hidden'],
                                     ['clue2 at p2 w/o hidden']]
                              },
@@ -47,5 +57,10 @@ game = {'chars': {'A': False, 'B': False, 'C': False},
         'stories': {'A': 'A story',
                     'B': 'B story',
                     'C': 'C story'},
+        'round_ap': 4,
         'player_num': 3
         }
+
+out_file = open("sample_game.json", "w")
+json.dump(game, out_file)
+out_file.close()
