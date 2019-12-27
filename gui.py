9fea836aec3f1e51bb9a2833a34e562ccd0a3eed
Yuhan Liu <liuyuhanheidi@163.com>
2019-12-26 19:42:10 -0500

    yl556 - connected to server; added security feature

--- gui.py
+++ gui.py
@@ -15,25 +15,25 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    from server import error_messages
     error_msg = None
     if request.method == "POST":
         if request.form["action"] == "signup":
             signed_up = server.new_user(request.form["username"],
                                         request.form["psw"])
             if not signed_up:
-                error_msg = "The user has existed"
+                error_msg = error_messages[1]
             else:
                 message = "You've signed up"
                 return render_template("login.html", msg1=message)
         elif request.form["action"] == "login":
-
             logged_in = server.verify_user(request.form["username"],
                                            request.form["psw"])
             if type(logged_in) == str:
                 error_msg = logged_in
             elif logged_in:
                 return redirect(url_for("choose_ch",
-                                        uname=request.form["username"]))
+                                        u_name=request.form["username"]))
     return render_template("login.html", error=error_msg)
 
 
@@ -42,21 +42,31 @@ def choose_ch(u_name):
     import database
     if request.method == "POST":
         ch_name = request.form["choose_ch"]
-        database.assign_character(u_name, ch_name)
-        return redirect(url_for("play", uname=u_name, chname=ch_name))
+        database.add_char(u_name, ch_name)
+        return redirect(url_for("play", u_name=u_name, ch_name=ch_name))
     else:
         ch_name = server.verify_ch(u_name)
         if ch_name is str:
-            return redirect(url_for("play", uname=u_name, chname=ch_name))
+            return redirect(url_for("play", u_name=u_name, ch_name=ch_name))
         else:
             return render_template("choose_ch.html")
 
 
 @app.route("/<u_name>/play/<ch_name>", methods=["GET", "POST"])
 def play(u_name, ch_name):
+    from database import user, game
+    from server import error_messages
+    message = None
     if request.method == "GET":
-        message = {"u_name": u_name, "ch_name": ch_name}
-        return render_template("play.html", msg1=message)
+        ch = user[u_name]["char"]["name"]
+        if ch_name == ch:
+            message = {"u_name": u_name, "ch_name": ch_name}
+            return render_template("play.html", msg_info=message)
+        else:
+            if ch_name in game['chars']:
+                return error_messages[5]
+            else:
+                return error_messages[6]
 
 
 if __name__ == "__main__":
