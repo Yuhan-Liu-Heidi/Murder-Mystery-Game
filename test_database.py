e5f7daba384105b5bcd23febcda0c7b31de51917
Lucy Liang <lucy.liang@duke.edu>
2019-12-26 20:48:39 -0500

    added test for add_char

--- test_database.py
+++ test_database.py
@@ -6,7 +6,7 @@ import pytest
     ('heidi',
      '1234',
      {'heidi': {'pw': '1234',
-                'char': {'name': None, 'story': None},
+                'char': None,
                 'ap': 4,
                 'clue': {},
                 'round': {1: False, 2: False}
@@ -14,13 +14,13 @@ import pytest
     ('lucy',
      '2345',
      {'heidi': {'pw': '1234',
-                'char': {'name': None, 'story': None},
+                'char': None,
                 'ap': 4,
                 'clue': {},
                 'round': {1: False, 2: False}
                 },
       'lucy': {'pw': '2345',
-               'char': {'name': None, 'story': None},
+               'char': None,
                'ap': 4,
                'clue': {},
                'round': {1: False, 2: False}
@@ -30,3 +30,42 @@ def test_create_user(user_id, pw, expected):
     from database import create_user
     user_dict = create_user(user_id, pw)
     assert user_dict == expected
+
+
+@pytest.mark.parametrize("user_id, char, expected_user, expected_track", [
+    ('lucy',
+     'A',
+     {'heidi': {'pw': '1234',
+                'char': None,
+                'ap': 4,
+                'clue': {},
+                'round': {1: False, 2: False}
+                },
+      'lucy': {'pw': '2345',
+               'char': 'A',
+               'ap': 4,
+               'clue': {},
+               'round': {1: False, 2: False}
+               }},
+     {'A': True, 'B': False, 'C': False}),
+    ('heidi',
+     'B',
+     {'heidi': {'pw': '1234',
+                'char': 'B',
+                'ap': 4,
+                'clue': {},
+                'round': {1: False, 2: False}
+                },
+      'lucy': {'pw': '2345',
+               'char': 'A',
+               'ap': 4,
+               'clue': {},
+               'round': {1: False, 2: False}
+               }},
+     {'A': True, 'B': True, 'C': False})
+])
+def test_add_char(user_id, char, expected_user, expected_track):
+    from database import add_char
+    user, track = add_char(user_id, char)
+    assert user == expected_user and \
+        track['chars'] == expected_track
