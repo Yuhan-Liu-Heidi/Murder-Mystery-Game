98557117263dc5189c72b08bbe171502b704c0b8
Lucy Liang <lucy.liang@duke.edu>
2019-12-26 20:48:02 -0500

    changed place names to length of 3

--- testdata.py
+++ testdata.py
@@ -3,55 +3,55 @@ import json
 user = {'01': {'pw': '111',
                'char': {'name': None, 'story': None},
                'ap': 4,
-               'clue': {'p1': [['this is 1/2 clue at p1 ele1',
-                                'this is 2/2 clue at p1 ele1'],
-                               ['this is 1/1 clue at p1 ele2']],
-                        'p2': [['this is 1/1 clue at p2 ele1'],
-                               ['this is 1/1 clue at p2 ele2']]},
+               'clue': {'p01': [['this is 1/2 clue at p1 ele1',
+                                 'this is 2/2 clue at p1 ele1'],
+                                ['this is 1/1 clue at p1 ele2']],
+                        'p02': [['this is 1/1 clue at p2 ele1'],
+                                ['this is 1/1 clue at p2 ele2']]},
                'round': {1: False, 2: False}
                },
         '02': {'pw': '222',
                'char': {'name': None, 'story': None},
                'ap': 4,
-               'clue': {'p1': [['this is 1/2 clue at p1 ele1',
-                                'this is 2/2 clue at p1 ele1'],
-                               ['this is 1/1 clue at p1 ele2']],
-                        'p2': [['this is 1/1 clue at p2 ele1'],
-                               ['this is 1/1 clue at p2 ele2']]},
+               'clue': {'p01': [['this is 1/2 clue at p1 ele1',
+                                 'this is 2/2 clue at p1 ele1'],
+                                ['this is 1/1 clue at p1 ele2']],
+                        'p02': [['this is 1/1 clue at p2 ele1'],
+                                ['this is 1/1 clue at p2 ele2']]},
                'round': {1: False, 2: False}
                },
         '03': {'pw': '333',
                'char': {'name': None, 'story': None},
                'ap': 4,
-               'clue': {'p1': [['this is 1/2 clue at p1 ele1',
-                                'this is 2/2 clue at p1 ele1'],
-                               ['this is 1/1 clue at p1 ele2']],
-                        'p2': [['this is 1/1 clue at p2 ele1'],
-                               ['this is 1/1 clue at p2 ele2']]},
+               'clue': {'p01': [['this is 1/2 clue at p1 ele1',
+                                 'this is 2/2 clue at p1 ele1'],
+                                ['this is 1/1 clue at p1 ele2']],
+                        'p02': [['this is 1/1 clue at p2 ele1'],
+                                ['this is 1/1 clue at p2 ele2']]},
                'round': {1: False, 2: False}
                }
         }
 
 track = {'chars': {'A': False, 'B': False, 'C': False},
-         'publicized_clue': {'p1': [['this is 1/2 clue at p1 ele1'],
-                                    ['this is 1/1 clue at p1 ele2']],
-                             'p2': [['this is 1/1 clue at p2 ele1']]},
+         'publicized_clue': {'p01': [['this is 1/2 clue at p1 ele1'],
+                                     ['this is 1/1 clue at p1 ele2']],
+                             'p02': [['this is 1/1 clue at p2 ele1']]},
          }
 
 game = {'chars': ['A', 'B', 'C'],
-        'clues': {'round1': {'p1': [['clue1 at p1 w/ hidden',
-                                     'hidden of clue1 at p1'],
+        'clues': {'round1': {'p01': [['clue1 at p1 w/ hidden',
+                                      'hidden of clue1 at p1'],
                              # w/ hidden, list with 2 elements
-                                    ['clue2 at p1 w/o hidden']],
+                                     ['clue2 at p1 w/o hidden']],
                              # w/o hidden, list with 1 element
-                             'p2': [['clue1 at p2 w/o hidden'],
-                                    ['clue2 at p2 w/o hidden']]
+                             'p02': [['clue1 at p2 w/o hidden'],
+                                     ['clue2 at p2 w/o hidden']]
                              },
-                  'round2': {'p1': [['clue1 at p1 w/ hidden',
-                                     'hidden of clue1 at p1'],
-                                    ['clue2 at p1 w/o hidden']],
-                             'p2': [['clue1 at p2 w/o hidden'],
-                                    ['clue2 at p2 w/o hidden']]
+                  'round2': {'p01': [['clue1 at p1 w/ hidden',
+                                      'hidden of clue1 at p1'],
+                                     ['clue2 at p1 w/o hidden']],
+                             'p02': [['clue1 at p2 w/o hidden'],
+                                     ['clue2 at p2 w/o hidden']]
                              }
                   },
         'stories': {'A': 'A story',
